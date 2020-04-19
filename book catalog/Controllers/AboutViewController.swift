60d7ac842b39e5237c03e38b54e36cdb81ed3410
Aldo Vernando <aldo.vernando@gmail.com>
2020-04-19 15:08:41 +0700

    Create app's UI

---
+++ book catalog/Controllers/AboutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutViewController.swift
+//  book catalog
+//
+//  Created by TEMP on 4/19/20.
+//  Copyright © 2020 aldovernando. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
